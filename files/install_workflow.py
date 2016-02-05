90c816bd40a4971dda8bd96d865efb1dee131566
Enis Afgan <afgane@gmail.com>
2016-02-05 17:49:45 -0500

    Make sure the opened workflow file gets closed after it's been loaded

--- files/install_workflow.py
+++ files/install_workflow.py
@@ -22,7 +22,8 @@ def main():
 
     gi = galaxy.GalaxyInstance(url=args.galaxy_url, key=args.api_key)
 
-    import_uuid = json.load(open(args.workflow_path, 'r')).get('uuid')
+    with open(args.workflow_path, 'r') as wf_file:
+        import_uuid = json.load(wf_file).get('uuid')
     existing_uuids = [d.get('latest_workflow_uuid') for d in gi.workflows.get_workflows()]
     if import_uuid not in existing_uuids:
         gi.workflows.import_workflow_from_local_path(args.workflow_path)
